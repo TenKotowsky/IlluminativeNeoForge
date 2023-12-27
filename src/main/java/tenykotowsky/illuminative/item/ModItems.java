3cd6772fc73efb75cbe4d9ea91f454cb8353d595
TenKotowsky <fairoutak@gmail.com>
2023-12-27 23:10:12 +0100

    Textures

--- src/main/java/tenykotowsky/illuminative/item/ModItems.java
+++ src/main/java/tenykotowsky/illuminative/item/ModItems.java
@@ -1,19 +1,16 @@
 package tenykotowsky.illuminative.item;
 
-import net.minecraft.core.registries.Registries;
 import net.minecraft.world.food.FoodProperties;
-import net.minecraft.world.food.Foods;
 import net.minecraft.world.item.Item;
 import net.neoforged.bus.api.IEventBus;
 import net.neoforged.neoforge.registries.DeferredItem;
 import net.neoforged.neoforge.registries.DeferredRegister;
-import net.neoforged.neoforge.registries.NeoForgeRegistries;
 import tenykotowsky.illuminative.Illuminative;
 
 public class ModItems {
     public static final DeferredRegister.Items ITEMS = DeferredRegister.createItems(Illuminative.MOD_ID);
 
-    public static final DeferredItem<Item> EXAMPLE_ITEM = ITEMS.registerSimpleItem("example_item", new Item.Properties().food(new FoodProperties.Builder()
+    public static final DeferredItem<Item> GLOW_FISH = ITEMS.registerSimpleItem("glow_fish", new Item.Properties().food(new FoodProperties.Builder()
             .nutrition(2).saturationMod(0.1F).build()));
 
     public static void register(IEventBus eventBus) {
