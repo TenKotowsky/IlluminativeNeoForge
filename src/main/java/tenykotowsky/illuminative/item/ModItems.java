9886cd46ab017c414367ccda87514fb6f321e3c2
TenKotowsky <fairoutak@gmail.com>
2023-12-28 12:10:16 +0100

    Craftings

--- src/main/java/tenykotowsky/illuminative/item/ModItems.java
+++ src/main/java/tenykotowsky/illuminative/item/ModItems.java
@@ -13,6 +13,9 @@ public class ModItems {
     public static final DeferredItem<Item> GLOW_FISH = ITEMS.registerSimpleItem("glow_fish", new Item.Properties().food(new FoodProperties.Builder()
             .nutrition(2).saturationMod(0.1F).build()));
 
+    public static final DeferredItem<Item> COOKED_GLOW_FISH = ITEMS.registerSimpleItem("cooked_glow_fish", new Item.Properties().food(new FoodProperties.Builder()
+            .nutrition(6).saturationMod(0.8F).build()));
+
     public static void register(IEventBus eventBus) {
         ITEMS.register(eventBus);
     }
